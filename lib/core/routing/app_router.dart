50bce39614afa9933d708debe23bb74dd3d102e1
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-14 21:17:24 +0200

    Remove unused arguments variable in route generation

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -6,7 +6,7 @@ import 'routes.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
-    final arguments = settings.arguments;
+    // final arguments = settings.arguments;
     switch (settings.name) {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(
