4737db931c2f3fc716526be0aa07bb90d768ae38
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-16 20:27:45 +0200

    Implement Forget Password screen layout with email input and verification button

--- lib/features/auth/forget_password/presentation/screen/forget_password_screen.dart
+++ lib/features/auth/forget_password/presentation/screen/forget_password_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../core/helpers/spacing.dart';
+import '../../../../../core/theme/styles.dart';
+import '../../../../../core/widgets/auth_text_form_field.dart';
+import '../../../../../core/widgets/stack_shape.dart';
+import '../../../get_started/presentation/widgets/custom_auth_button.dart';
 
 class ForgetPasswordScreen extends StatelessWidget {
   const ForgetPasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const StackShape(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              child: Column(
+                children: [
+                  verticalSpacing(30),
+                  Image.asset(
+                    'assets/images/Watfa_logo 4.png',
+                    width: 170.w,
+                    fit: BoxFit.fitWidth,
+                  ),
+                  verticalSpacing(40),
+                  Text("Forgot Password",
+                      style: TextStyles.font24Blackw700Roboto),
+                  verticalSpacing(10),
+                  Text("Please enter your email to reset the password",
+                      style: TextStyles.font14Grayw400Roboto),
+                  verticalSpacing(40),
+                  AuthTextFormField(
+                    prefixIcon: 'assets/svgs/email_text_field_icon.svg',
+                    hintText: 'Email',
+                    controller: TextEditingController(),
+                  ),
+                  verticalSpacing(60),
+                  CustomAuthButton(
+                    onTap: () {},
+                    text: 'Verify',
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
