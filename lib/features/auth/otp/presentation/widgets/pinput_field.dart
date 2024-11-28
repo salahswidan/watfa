dbff6afeecba5c6df2943ec2b8d25e7e9a33cde1
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-28 17:20:28 +0200

    Refactor PinputField widget to enhance UI, adjust pin length to 4, and update styling with responsive dimensions

--- lib/features/auth/otp/presentation/widgets/pinput_field.dart
+++ lib/features/auth/otp/presentation/widgets/pinput_field.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:gradient_borders/gradient_borders.dart';
 import 'package:pinput/pinput.dart';
+import 'package:watfa/core/helpers/extinsions.dart';
 
 import '../../../../../core/theme/colors.dart';
-import '../../../../../core/theme/styles.dart';
 
 class PinputField extends StatelessWidget {
   const PinputField({
@@ -12,28 +11,44 @@ class PinputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Pinput(
-      length: 6,
-      defaultPinTheme: PinTheme(
-        height: 50,
-        width: 335,
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16),
-          border: const GradientBoxBorder(
-            gradient: LinearGradient(
-              colors: [
-                ColorsManagers.voliet,
-                ColorsManagers.purple,
-              ],
-            ),
-            width: 1.5,
+    return Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 20.w(context)),
+      child: Pinput(
+        length: 4,
+        defaultPinTheme: PinTheme(
+          height: 60,
+          width: context.screenWidth - 120.w(context),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: ColorsManagers.platinum,
+            borderRadius: BorderRadius.circular(16),
+            
           ),
         ),
+        focusedPinTheme: PinTheme(
+          height: 60,
+          width: context.screenWidth - 120.w(context),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: ColorsManagers.platinum,
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(
+              color: ColorsManagers.purple,
+            ),
+          ),
+        ),
+        followingPinTheme: PinTheme(
+          height: 60,
+          width: context.screenWidth - 120.w(context),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: ColorsManagers.platinum,
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        controller: TextEditingController(),
+        onCompleted: (value) {},
       ),
-      controller: TextEditingController(),
-      onCompleted: (value) {},
     );
   }
 }
