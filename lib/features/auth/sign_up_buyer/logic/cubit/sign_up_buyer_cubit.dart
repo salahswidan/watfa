f47ed6aa417c266fce36bcbc646755192526afe5
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-03 21:04:07 +0200

    Add SignUpBuyerRepo and SignUpBuyerCubit for user registration functionality

---
+++ lib/features/auth/sign_up_buyer/logic/cubit/sign_up_buyer_cubit.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:watfa/core/helpers/constants.dart';
+import 'package:watfa/features/auth/sign_up_buyer/data/model/sign_up_buyer_request_body.dart';
+
+import '../../data/repo/sign_up_buyer_repo.dart';
+import 'sign_up_buyer_state.dart';
+
+class SignUpBuyerCubit extends Cubit<SignUpBuyerState> {
+  final SignUpBuyerRepo _signUpBuyerRepo;
+  SignUpBuyerCubit(
+    this._signUpBuyerRepo,
+  ) : super(SignUpBuyerState.initial());
+
+  TextEditingController userNameOrEmailController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  void emitSignUpBuyerState() async {
+    emit(const SignUpBuyerState.loading());
+    final response = await _signUpBuyerRepo.signUpBuyer(
+      SignUpBuyerRequestBody(
+        userNameOrEmail: userNameOrEmailController.text,
+        phoneNumber: phoneNumberController.text,
+        password: passwordController.text,
+        confirmPassword: confirmPasswordController.text,
+        role: 'buyer',
+        country: Constants.selectedCountryname,
+      ),
+    );
+    response.when(
+      success: (data) {
+        emit(SignUpBuyerState.success(data));
+      },
+      failure: (apiErrorModel) {
+        emit(SignUpBuyerState.error(apiErrorModel));
+      },
+    );
+  }
+}
