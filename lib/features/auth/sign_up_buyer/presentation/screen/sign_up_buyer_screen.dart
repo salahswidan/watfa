d5c288de5f7c4953776fdcebf378b9c81ce024a7
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-03 21:04:45 +0200

    Refactor SignUpBuyerScreen to use FixedData and SignUpBuyerForm components; add SignUpBuyerBlocListener for handling sign-up state changes

--- lib/features/auth/sign_up_buyer/presentation/screen/sign_up_buyer_screen.dart
+++ lib/features/auth/sign_up_buyer/presentation/screen/sign_up_buyer_screen.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../../../core/di/dependency_injection.dart';
 import '../../../../../core/helpers/spacing.dart';
 import '../../../../../core/theme/styles.dart';
-import '../../../../../core/widgets/auth_text_form_field.dart';
 import '../../../../../core/widgets/custom_gradient_container.dart';
-import '../../../countryPicker/logic/cubit/get_all_countries_cubit.dart';
-import '../../../countryPicker/ui/country_picker.dart';
-import '../../../get_started/presentation/widgets/custom_auth_button.dart';
 import '../../../login/presentation/section/social_auth.dart';
-import '../../../login/presentation/widget/logo.dart';
 import '../section/already_have_an_account.dart';
-import '../section/password_listener.dart';
+import '../section/fixed_data.dart';
+import '../section/sign_up_buyer_bloc_listener.dart';
+import '../section/sign_up_buyer_form.dart';
 
 class SignUpBuyerScreen extends StatelessWidget {
   const SignUpBuyerScreen({super.key});
@@ -28,85 +23,11 @@ class SignUpBuyerScreen extends StatelessWidget {
               SliverList(
                 delegate: SliverChildListDelegate(
                   [
-                    verticalSpacing(60),
-                    const Logo(),
-                    verticalSpacing(28),
-                    Center(
-                      child: Text("Let’s Get Started!",
-                          style: TextStyles.font24Blackw700Roboto(context)),
-                    ),
-                    verticalSpacing(18),
-                    Center(
-                      child: Text("Create an account  to get all features",
-                          style:
-                              TextStyles.font14DarkSilverw400Roboto(context)),
-                    ),
-                    verticalSpacing(30),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Username or email",
-                        style: TextStyles.font14Jetw500Poppins(context),
-                      ),
-                    ),
-                    AuthTextFormField(
-                      suffixIcon: 'assets/svgs/username_text_field_icon.svg',
-                      hintText: 'Mariam Fawzy',
-                      controller: TextEditingController(),
-                    ),
-                    verticalSpacing(16),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Phone number",
-                        style: TextStyles.font14Jetw500Poppins(context),
-                      ),
-                    ),
-                    IntrinsicHeight(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          BlocProvider(
-                            create: (context) => getit<GetAllCountriesCubit>()
-                              ..getAllCountries(),
-                            child: const CountryPicker(),
-                          ),
-                        ],
-                      ),
-                    ),
-                    verticalSpacing(16),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Password",
-                        style: TextStyles.font14Jetw500Poppins(context),
-                      ),
-                    ),
-                    AuthTextFormField(
-                      isPassword: true,
-                      hintText: 'Password',
-                      controller: TextEditingController(),
-                    ),
-                    verticalSpacing(16),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Confirm Password",
-                        style: TextStyles.font14Jetw500Poppins(context),
-                      ),
-                    ),
-                    AuthTextFormField(
-                      isPassword: true,
-                      hintText: 'Confirm Password',
-                      controller: TextEditingController(),
-                    ),
-                    verticalSpacing(10),
-                    const PasswordListener(),
+                    FixedData(),
+                    SignUpBuyerForm(),
                     verticalSpacing(25),
-                    CustomAuthButton(
-                      onTap: () {},
-                      text: 'Sign Up',
-                    ),
+                    
+                    SignUpBuyerBlocListener(),
                     verticalSpacing(40),
                     Center(
                       child: Text(
@@ -129,4 +50,5 @@ class SignUpBuyerScreen extends StatelessWidget {
       ),
     );
   }
+
 }
