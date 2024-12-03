f4b90b58518c63fb8e4b7735f763ff359ae9556c
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-03 20:56:42 +0200

    Refactor dependency injection to use consistent naming and add SignUpBuyer dependencies

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -4,19 +4,25 @@ import 'package:get_it/get_it.dart';
 import '../../features/auth/countryPicker/data/repo/country_picker_repo.dart';
 import '../../features/auth/countryPicker/logic/cubit/get_all_countries_cubit.dart';
 
+import '../../features/auth/sign_up_buyer/data/repo/sign_up_buyer_repo.dart';
+import '../../features/auth/sign_up_buyer/logic/cubit/sign_up_buyer_cubit.dart';
 import '../network/api_service.dart';
 import '../network/dio_factory.dart';
 
-final getit = GetIt.instance;
+final getIt = GetIt.instance;
 
 Future<void> setupGetIT() async {
   // Dio & ApiService
   Dio dio = DioFactory.getDio();
-  getit.registerLazySingleton<ApiService>(() => ApiService(dio));
+  getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
 
   // Country Picker
-  getit.registerLazySingleton<CountryPickerRepo>(
-      () => CountryPickerRepo(getit()));
-  getit.registerFactory<GetAllCountriesCubit>(
-      () => GetAllCountriesCubit(getit()));
+  getIt.registerLazySingleton<CountryPickerRepo>(
+      () => CountryPickerRepo(getIt()));
+  getIt.registerFactory<GetAllCountriesCubit>(
+      () => GetAllCountriesCubit(getIt()));
+
+  // SignUpBuyer
+  getIt.registerLazySingleton<SignUpBuyerRepo>(() => SignUpBuyerRepo(getIt()));
+  getIt.registerFactory<SignUpBuyerCubit>(() => SignUpBuyerCubit(getIt()));
 }
