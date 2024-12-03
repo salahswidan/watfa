320fa3aebf897511ef5f4424589a6c92a288eed3
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-03 21:02:45 +0200

    Add FixedData widget for user registration screen with introductory text

---
+++ lib/features/auth/sign_up_buyer/presentation/section/fixed_data.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/helpers/spacing.dart';
+import '../../../../../core/theme/styles.dart';
+import '../../../login/presentation/widget/logo.dart';
+
+class FixedData extends StatelessWidget {
+  const FixedData({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        verticalSpacing(60),
+        const Logo(),
+        verticalSpacing(28),
+        Center(
+          child: Text("Let’s Get Started!",
+              style: TextStyles.font24Blackw700Roboto(context)),
+        ),
+        verticalSpacing(18),
+        Center(
+          child: Text("Create an account  to get all features",
+              style: TextStyles.font14DarkSilverw400Roboto(
+                  context)),
+        ),
+        verticalSpacing(30),
+      ],
+    );
+  }
+}
