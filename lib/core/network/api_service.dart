4cf96742ad11a1ce4e49ee89ca2311ddc105f07e
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-04 22:53:13 +0200

    Add login method to ApiService for user authentication

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 
 import '../../features/auth/countryPicker/data/models/all_country_with_flag.dart';
+import '../../features/auth/login/data/model/login_request_body.dart';
 import '../../features/auth/sign_up_buyer/data/model/sign_up_buyer_request_body.dart';
 import '../../features/auth/sign_up_buyer/data/model/sign_up_buyer_response.dart';
 import 'api_constants.dart';
@@ -15,6 +16,11 @@ abstract class ApiService {
   @POST(ApiConstants.register)
   Future<SignUpBuyerResponse> signup(
     @Body() SignUpBuyerRequestBody signUpBuyerRequestBody,
+  );
+
+  @POST(ApiConstants.login)
+  Future<SignUpBuyerResponse> login(
+    @Body() LoginRequestBody loginRequestBody,
   );
 
   @GET(ApiConstants.allCountries)
--- lib/core/network/api_service.g.dart
+++ lib/core/network/api_service.g.dart
@@ -59,6 +59,40 @@ class _ApiService implements ApiService {
   }
 
   @override
+  Future<SignUpBuyerResponse> login(LoginRequestBody loginRequestBody) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(loginRequestBody.toJson());
+    final _options = _setStreamType<SignUpBuyerResponse>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'auth/login',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late SignUpBuyerResponse _value;
+    try {
+      _value = SignUpBuyerResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
   Future<AllCountryWithFlagResponese> getAllCountries() async {
     final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
