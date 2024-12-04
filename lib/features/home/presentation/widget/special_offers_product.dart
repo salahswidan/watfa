25d51f922243f128034a276fc9b6a20abd2d992b
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-04 22:54:14 +0200

    Add SpecialOffersProduct widget for displaying promotional products

---
+++ lib/features/home/presentation/widget/special_offers_product.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:watfa/core/helpers/extinsions.dart';
+
+import '../../../../core/theme/colors.dart';
+import '../../../../core/theme/styles.dart';
+
+class SpecialOffersProduct extends StatelessWidget {
+  const SpecialOffersProduct({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 245.h(context),
+      width: 185.w(context),
+      decoration: BoxDecoration(
+        color: ColorsManagers.lavenderBlush,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 3),
+                      decoration: BoxDecoration(
+                          color: ColorsManagers.purple,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                          )),
+                      child: Text("45% OFF",
+                          style: TextStyles.font10WhiteW600Manrope(context)),
+                    ),
+                    Icon(
+                      Icons.bookmark_border_outlined,
+                      color: ColorsManagers.blackOlive,
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Text(
+                  "SAR 230.00",
+                  style: TextStyles.font20BlackOliveW700Manrope(context),
+                ),
+                Text(
+                  "SAR 512.58",
+                  style:
+                      TextStyles.font12BlackOliveW400Manrope(context).copyWith(
+                    decoration: TextDecoration.lineThrough,
+                  ),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.star,
+                      size: 12,
+                      color: ColorsManagers.sunRay,
+                    ),
+                    Text("4.5",
+                        style: TextStyles.font12BlackOliveW400Manrope(context)),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: -16.h(context),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/product.png",
+                  width: 140.w(context),
+                ),
+                Text("lip gloss",
+                    style: TextStyles.font14BlackwOliveW400Manrope(context)),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
