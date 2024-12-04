674d1b5ff63d1192edea189950ca6d9980b9ec8a
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-04 22:54:28 +0200

    Add DefaultProduct widget for displaying product details in the HomeScreen

---
+++ lib/features/home/presentation/widget/default_product.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:watfa/core/helpers/extinsions.dart';
+import 'package:watfa/core/helpers/spacing.dart';
+
+import '../../../../core/theme/colors.dart';
+import '../../../../core/theme/styles.dart';
+
+class DefaultProduct extends StatelessWidget {
+  final String image, text;
+  const DefaultProduct({
+    super.key,
+    required this.image,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250.h(context),
+      width: 185.w(context),
+      decoration: BoxDecoration(
+        color: ColorsManagers.antiFlashWhite,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 6, vertical: 3),
+                  decoration: BoxDecoration(
+                      color: ColorsManagers.purple,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      )),
+                  child: Text("45% OFF",
+                      style: TextStyles.font10WhiteW600Manrope(context)),
+                ),
+                Icon(
+                  Icons.bookmark_border_outlined,
+                  color: ColorsManagers.blackOlive,
+                ),
+              ],
+            ),
+            Flexible(child: verticalSpacing(5.h(context))),
+            Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(
+                  image,
+                  height: 105.h(context),
+                  width: 140.w(context),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Flexible(child: verticalSpacing(5.h(context))),
+            Center(
+                child: Text(text,
+                    style: TextStyles.font14BlackwOliveW400Manrope(context))),
+            Expanded(child: SizedBox()),
+            Text(
+              "SAR 230.00",
+              style: TextStyles.font20BlackOliveW700Manrope(context),
+            ),
+            Text(
+              "SAR 512.58",
+              style: TextStyles.font12BlackOliveW400Manrope(context).copyWith(
+                decoration: TextDecoration.lineThrough,
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Icon(
+                  Icons.star,
+                  size: 12,
+                  color: ColorsManagers.sunRay,
+                ),
+                Text("4.9",
+                    style: TextStyles.font12BlackOliveW400Manrope(context)),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
