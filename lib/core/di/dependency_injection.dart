9dd12fcbd0535d2f87e0d75284cc85cb0673dd38
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-05 14:06:13 +0200

    Add LoginRepo and LoginCubit to dependency injection setup

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -4,6 +4,8 @@ import 'package:get_it/get_it.dart';
 import '../../features/auth/countryPicker/data/repo/country_picker_repo.dart';
 import '../../features/auth/countryPicker/logic/cubit/get_all_countries_cubit.dart';
 
+import '../../features/auth/login/data/repo/login_repo.dart';
+import '../../features/auth/login/logic/cubit/login_cubit.dart';
 import '../../features/auth/sign_up_buyer/data/repo/sign_up_buyer_repo.dart';
 import '../../features/auth/sign_up_buyer/logic/cubit/sign_up_buyer_cubit.dart';
 import '../network/api_service.dart';
@@ -25,4 +27,8 @@ Future<void> setupGetIT() async {
   // SignUpBuyer
   getIt.registerLazySingleton<SignUpBuyerRepo>(() => SignUpBuyerRepo(getIt()));
   getIt.registerFactory<SignUpBuyerCubit>(() => SignUpBuyerCubit(getIt()));
+
+  // login
+   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
 }
