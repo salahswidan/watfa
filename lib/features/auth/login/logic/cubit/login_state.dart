abc8ac383a79dd9d2f004d9ed3200becac54f792
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-05 16:27:59 +0200

    Refactor LoginState to be generic, allowing success state to carry data

--- lib/features/auth/login/logic/cubit/login_state.dart
+++ lib/features/auth/login/logic/cubit/login_state.dart
@@ -1,9 +1,9 @@
 part of 'login_cubit.dart';
 
 @freezed
-class LoginState with _$LoginState {
+class LoginState<T> with _$LoginState {
   const factory LoginState.initial() = _Initial;
   const factory LoginState.loading() = LoginLoading;
-  const factory LoginState.success() = LoginSuccess;
+  const factory LoginState.success(T data) = LoginSuccess<T>;
   const factory LoginState.error(ApiErrorModel apiErrorModel) = LoginError;
 }
