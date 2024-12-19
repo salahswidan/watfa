1a0ccac4f2b3cbb435b2967312e38e4ecfd69c79
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-19 18:42:35 +0200

    Add AvailableWallet widget for displaying wallet information

---
+++ lib/features/wallet/presentation/widget/available_wallet.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/helpers/spacing.dart';
+import '../../../../core/theme/colors.dart';
+import '../../../../core/theme/styles.dart';
+
+
+class AvailableWallet extends StatelessWidget {
+  const AvailableWallet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(color: ColorsManagers.purple),
+        ),
+        padding: const EdgeInsets.all(25),
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/wallet screen.png",
+                width: 40,
+              ),
+              verticalSpacing(30),
+              Text(
+                "Available to use",
+                style:
+                    TextStyles.font15RussianVioletW600Manrope(context),
+              ),
+              verticalSpacing(15),
+              Text(
+                "AED 0.00",
+                style: TextStyles.font32GraniteGrayW700Manrope(context),
+              ),
+              verticalSpacing(20),
+            ],
+          ),
+        ));
+  }
+}
