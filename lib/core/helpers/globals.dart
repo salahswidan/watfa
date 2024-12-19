bae62c8bd63d93676752ff44bb40bbb829f4cd1e
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-19 18:42:51 +0200

    Refactor settingsCardList to use a function and add navigation for Wallet and Notification

--- lib/core/helpers/globals.dart
+++ lib/core/helpers/globals.dart
@@ -1,30 +1,37 @@
-import '../../features/settings/logic/models/card_model.dart';
+import 'package:flutter/material.dart';
+import 'package:watfa/core/helpers/extinsions.dart';
 
-List<CardModel> settingsCardList = [
-  CardModel(
-    title: 'Wallet',
-    image: 'assets/images/Wallet.png',
-    onTap: () {},
-  ),
-  CardModel(
-    title: 'Payment Methods',
-    image: 'assets/images/Bank Cards.png',
-    onTap: () {},
-  ),
-  CardModel(
-    title: 'Bank account',
-    image: 'assets/images/Merchant Account.png',
-    onTap: () {},
-  ),
-  CardModel(
-    title: 'Saved items',
-    image: 'assets/images/Bookmark.png',
-    onTap: () {},
-  ),
-  CardModel(
-    title: 'Notification',
-    image: 'assets/images/Alarm.png',
-    onTap: () {},
-  ),
- 
-];
+import '../../features/settings/logic/models/card_model.dart';
+import '../routing/routes.dart';
+
+List<CardModel> settingsCardList(BuildContext context) => [
+      CardModel(
+        title: 'Wallet',
+        image: 'assets/images/Wallet.png',
+        onTap: () {
+          context.pushNamed(Routes.walletScreen);
+        },
+      ),
+      CardModel(
+        title: 'Payment Methods',
+        image: 'assets/images/Bank Cards.png',
+        onTap: () {},
+      ),
+      CardModel(
+        title: 'Bank account',
+        image: 'assets/images/Merchant Account.png',
+        onTap: () {},
+      ),
+      CardModel(
+        title: 'Saved items',
+        image: 'assets/images/Bookmark.png',
+        onTap: () {},
+      ),
+      CardModel(
+        title: 'Notification',
+        image: 'assets/images/Alarm.png',
+        onTap: () {
+          context.pushNamed(Routes.notificationsScreen);
+        },
+      ),
+    ];
