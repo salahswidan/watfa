96dad7370bb3a789f8b7dd0999d79bd6093672b9
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-12-26 23:50:52 +0200

    Add installment selection components to SelectPlanScreen; integrate SelectedInstallmentContainer and UnSelectedInstallmentContainer

--- lib/features/select_plan/presentation/screen/select_plan_screen.dart
+++ lib/features/select_plan/presentation/screen/select_plan_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:watfa/core/helpers/spacing.dart';
 import 'package:watfa/core/theme/styles.dart';
 import 'package:watfa/core/widgets/default_app_bar.dart';
 
 import '../../../../core/theme/colors.dart';
+import '../widgets/selected_installment_container.dart';
+import '../widgets/un_selected_installment_container.dart';
 
 class SelectPlanScreen extends StatelessWidget {
   const SelectPlanScreen({super.key});
@@ -13,7 +16,6 @@ class SelectPlanScreen extends StatelessWidget {
       appBar: DefaultAppBar(
         hasBackArrow: true,
         title: "Payment by Watfa",
-        
       ),
       backgroundColor: ColorsManagers.cultured,
       body: Padding(
@@ -22,18 +24,23 @@ class SelectPlanScreen extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 40),
-              child: Text("Equal Installment Over 3 Months",style: TextStyles.font18BlackW600Roboto(context),),
+              child: Text(
+                "Equal Installment Over 3 Months",
+                style: TextStyles.font18BlackW600Roboto(context),
+              ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(6),
-                border: Border.all(color: ColorsManagers.africanViolet, width: 3),
+            SelectedInstallmentContainer(),
+            verticalSpacing(20),
+            UnSelectedInstallmentContainer(),
+            verticalSpacing(20),
+            UnSelectedInstallmentContainer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                "Payment Method",
+                style: TextStyles.font18RaisinBlackw600Ralway(context),
               ),
-              child: Row(
-                children: [],
-              ),
-            )
+            ),
           ],
         ),
       ),
